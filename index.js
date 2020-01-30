c04f65c4c2517dce7098fee8916308538629409a
Maicon Henrique Rodrigues do Nascimento <maiconhrn@hotmail.com>
2020-01-29 21:53:34 -0300

    Ajustes codigo desnecessario

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ io.on('connection', socket => {
 });
 
 mongoose.connect(process.env.MONGO_URL,
-    { useNewUrlParser: true });
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
 
 app.use((req, res, next) => {
     req.io = io;
@@ -30,6 +33,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-server.listen(process.env.PORT || 3333, () => {
+server.listen(process.env.PORT, () => {
     console.log('Server listening on port 3333');
 });
